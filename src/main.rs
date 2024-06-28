db0a0163cd9a8ad525391c43db50db5c722709df
qb <phao0724@163.com>
2024-06-28 17:21:42 +0800

    refactor(): Switch to io::File trait instead of File enum

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,22 @@
 use std::env;
-use std::fs;
 use std::process;
 pub mod symbolic;
 
 fn main() {
     println!("Symbolic, the language that has no keywords");
-    let args = symbolic::Args::from_args(env::args()).unwrap_or_else(|_err| {
+    let mut args = symbolic::Args::from_args(env::args()).unwrap_or_else(|_err| {
         println!("Invalid args");
         process::exit(1);
     });
 
-    println!("{} {}{}", match args.step {
-        symbolic::Step::Run => "Running",
-        symbolic::Step::Parse => "Parsing",
-        symbolic::Step::Compile => "Compiling",
-        symbolic::Step::Tokenize => "Tokenizing",
-    }, match args.input {
-        symbolic::File::StdIo => String::from("stdin"),
-        symbolic::File::File(ref name) => format!("file {}", name),
-    }, match args.output {
-        None => String::from(""),
-        Some(file) => match file {
-            symbolic::File::StdIo => String::from(" to stdio"),
-            symbolic::File::File(name1) => format!(" to file {}", name1),
-        }
+    println!("{}", match args.to {
+        symbolic::IR::Output => "Running",
+        symbolic::IR::AST => "Parsing",
+        symbolic::IR::Bytecode => "Compiling",
+        symbolic::IR::TokenStream => "Tokenizing",
+        symbolic::IR::Src => {panic!("What's the fxxk")}
     });
-    let filename = match args.input {
-        symbolic::File::StdIo => String::from("-"),
-        symbolic::File::File(name) => name,
-    };
-    let data = String::from_utf8(fs::read::<String>(filename).unwrap_or_else(|_err| {
-        println!("Cannot read");
-        process::abort();
-    })).expect("Cannot convert to utf8");
+    let mut data : String= Default::default();
+    args.input.read_to_string(&mut data);
     println!("Input file data:\n{}", data);
 }
--- src/symbolic/mod.rs
+++ src/symbolic/mod.rs
@@ -1,69 +1,111 @@
-use std::option::Option;
+use std::io::Read;
+use std::io::Write;
 
-pub enum Step {
-    Run,
-    Compile,
-    Parse,
-    Tokenize,
+/// Enum to indicate IRs in the progress.
+#[derive(PartialEq)]
+pub enum IR {
+    Output,
+    Bytecode,
+    AST,
+    TokenStream,
+    Src,
 }
 
-pub enum File {
-    StdIo,
-    File(String),
-}
-
+/// Struct of argumentss.
 pub struct Args {
-    pub step : Step,
-    pub input : File,
-    pub output : Option<File>,
+    pub from: IR,
+    pub to: IR,
+    pub input: Box<dyn Read>,
+    pub output: Box<dyn Write>,
+    pub optimize_config:OptConfig,
 }
 
-pub enum FailCase {
-    ParseargMultiFilename,
+pub struct OptConfig {
+    pub constant_fold: bool,
+    pub remove_dead_code: bool,
+}
+
+pub enum SymbolicError {
+    ParseargTooManyFiles,
     ParseargInvalidOption,
 }
 
+impl PartialOrd for IR {
+    fn partial_cmp(&self, rhs:&Self) -> Option<std::cmp::Ordering> {
+        let a_val = match self {IR::Output=>4, IR::Bytecode=>3, IR::AST=>2, IR::TokenStream=>1, IR::Src=>0};
+        let b_val = match rhs {IR::Output=>4, IR::Bytecode=>3, IR::AST=>2, IR::TokenStream=>1, IR::Src=>0};
+        a_val.partial_cmp(&b_val)
+    }
+}
+
 impl Args {
-    pub fn from_args(mut args: impl Iterator<Item = String>) -> Result<Args, FailCase>{
-        args.next();
+    /// Parse arguments from a iterator.
+    ///
+    /// The argument format is:
+    /// ```bash
+    /// symbolic <OPTS> [<INPUT> [<OUTPUT>]]
+    /// ```
+    pub fn from_args(mut args: impl Iterator<Item = String>) -> Result<Args, SymbolicError> {
+        args.next().ok_or(SymbolicError::ParseargInvalidOption)?;
         let mut retval = Args {
-            step:Step::Run,
-            input:File::StdIo,
-            output:None,
+            from: IR::Src,
+            to: IR::Output,
+            input: Box::new(std::io::stdin()),
+            output: Box::new(std::io::stdout()),
+            optimize_config: OptConfig {
+                constant_fold: false,
+                remove_dead_code: false,
+            },
         };
+        let mut retval_to_associated = false;
+        let mut retval_from_associated = false;
         while let Some(arg) = args.next() {
             match &arg as &str {
-                "parse" => {
-                    retval.step=Step::Run;
-                    retval.output=Some(File::StdIo);
+                "--from" => {
+                    retval.from = match &args.next().ok_or(SymbolicError::ParseargInvalidOption)? as &str {
+                        "src" | "source" => IR::Src,
+                        "tokens" => IR::TokenStream,
+                        "ast" => IR::AST,
+                        "bytecode" => IR::Bytecode,
+&_ => {return Err(SymbolicError::ParseargInvalidOption)},
+                    }
                 }
-                "tokenize" => {
-                    retval.step=Step::Tokenize;
-                    retval.output=Some(File::StdIo);
-                }
-                "compile" => {
-                    retval.step=Step::Compile;
-                    retval.output=Some(File::StdIo);
-                }
-                "run" => {
-                }
-                "--output" => {
-                    retval.output=Some(File::File(args.next().expect("6")));
+                "--to" => {
+                    retval.to = match &args.next().ok_or(SymbolicError::ParseargInvalidOption)? as &str {
+                        "tokens" => IR::TokenStream,
+                        "ast" => IR::AST,
+                        "bytecode" => IR::Bytecode,
+&_ => {return Err(SymbolicError::ParseargInvalidOption)},
+                    }
                 }
                 filename => {
-                    if filename.starts_with("--") {
-                        return Err(FailCase::ParseargInvalidOption);
+                    if retval_to_associated {
+                        // All 2 files are associated => FAIL
+                        return Err(SymbolicError::ParseargTooManyFiles);
                     }
-                    retval.input = if filename == "-" {
-                        File::StdIo
+                    if retval_from_associated {
+                    let associated_file : Box<dyn Write > = match filename {
+                        "-" => Box::new(std::io::stdout()),
+                        _ => {
+                            Box::new(std::fs::File::create(filename).unwrap())
+                        }
+                    };
+                        retval.output = associated_file;
+                        retval_to_associated = true;
                     } else {
-                        match retval.input { File::File(_) => {
-                            return Err(FailCase::ParseargMultiFilename);
-                        }, _ => ()}
-                        File::File(String::from(filename))
+                     let associated_file : Box <dyn Read> = match filename {
+                       "-" => Box::new(std::io::stdin()),
+                        _ => {
+                            Box::new(std::fs::File::open(filename).unwrap())
+                        }};
+                        retval.input = associated_file;
+                        retval_from_associated = true;
                     }
                 }
             }
+        }
+        if retval.from > retval.to {
+            return Err(SymbolicError::ParseargInvalidOption);
         }
         Ok(retval)
     }
